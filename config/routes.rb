3f3a2af9a7f4a7018d9a4ac0558b350e0284fb66
titas.norkunas <titas.norkunas@gmail.com>
2014-03-10 08:12:02 -1000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Datingapp::Application.routes.draw do
   resources :profiles
+  root 'profiles#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

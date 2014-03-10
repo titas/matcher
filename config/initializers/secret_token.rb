9cff1cc1cca27aaf95a97ebd5781c392de449fb3
titas.norkunas <titas.norkunas@gmail.com>
2014-03-10 08:09:46 -1000

    Blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Datingapp::Application.config.secret_key_base = 'cf8496ac2ff16c8c3bf0f619333ff106eb6f1c6bd5deee26df909dd1a66edad6784574f1f77e178e02c15464f50ec440f7d09230faf8a61afe015f7e91e84b9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Datingapp::Application.config.session_store :cookie_store, key: '_datingapp_session'
